c8e3687961d47667cf230bbd9ade24129a54a1c5
lana-z <lana@levelupeconomy.com>
2023-12-03 15:18:32 -0800

    changed

--- javascript/code-challenges/challenges-01.test.js
+++ javascript/code-challenges/challenges-01.test.js
@@ -67,7 +67,9 @@ Use `forEach` to build a new array of strings, each string modified by the callb
 ------------------------------------------------------------------------------------------------ */
 
 const greeting = (word) => {
-  // Solution code here...
+  let lanasString = ''
+
+  arr.forEach()
 };
 
 const speaker = (words, callback) => {

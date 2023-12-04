9f806f6277baa29391cfb54e87252b4dc3df368d
lana-z <lana@levelupeconomy.com>
2023-12-03 17:12:16 -0800

    challenge 3

--- javascript/code-challenges/challenges-03.test.js
+++ javascript/code-challenges/challenges-03.test.js
@@ -40,7 +40,7 @@ For example, containsAnd(['panda', 'ran', 'and']) returns ['panda', 'and'].
 ------------------------------------------------------------------------------------------------ */
 
 const containsAnd = (arr) => {
-  // Solution code here...
+  return arr.filter((hasAnd) => hasAnd.includes('and'));
 };
 
 /* ------------------------------------------------------------------------------------------------

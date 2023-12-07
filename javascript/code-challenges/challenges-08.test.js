7d62f27f5536a1c1e950c1d343181e7e46289f83
lana-z <lana@levelupeconomy.com>
2023-12-06 22:34:20 -0800

    challenge 3

--- javascript/code-challenges/challenges-08.test.js
+++ javascript/code-challenges/challenges-08.test.js
@@ -55,7 +55,7 @@ Write a function named joinArray that takes an array and joins all of the elemen
 ------------------------------------------------------------------------------------------------ */
 
 const joinArray = (arr) => {
-  // Solution code here...
+  return arr.join(' ');
 };
 
 /* ------------------------------------------------------------------------------------------------

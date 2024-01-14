d8621dd3a7fac5cfa146a06efd191bd319a2888f
lana-z <lana@levelupeconomy.com>
2024-01-13 17:14:55 -0800

    5 tests passing

--- python/data_structures/linked_list.py
+++ python/data_structures/linked_list.py
@@ -3,17 +3,19 @@ class LinkedList:
     def __init__(self):
         self.head = None
 
-
     def insert(self, value):
         new_node = Node(value)
         new_node.next = self.head
         self.head = new_node
-
-    def include
+        
 
     def __str__(self):
         values = []
         current = self.head
+        while current:
+            values.append(str(current.value))
+            current = current.next
+        return "NULL"
 
 
 class Node:

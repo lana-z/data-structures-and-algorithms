418651fc5a34cbe2feb2d1dd655acc50510c5ee4
lana-z <lana@levelupeconomy.com>
2024-01-21 21:37:08 -0800

    stack and queue

--- python/data_structures/queue.py
+++ python/data_structures/queue.py
@@ -1,12 +1,54 @@
+from data_structures.linked_list import Node
+
 class Queue:
-    """
-    Put docstring here
-    """
 
     def __init__(self):
-        # initialization here
-        pass
+        self.front = None
+        self.rear = None
 
-    def some_method(self):
-        # method body here
-        pass
+
+
+    def enqueue(self, value):
+        #adds a new node with that value to the back of the queue with the provided value
+        new_node = Node(value)
+
+        #if queue is empty, add new node to the front
+        if self.rear is None:
+            self.front = new_node
+            self.rear = new_node
+
+       #if queue is not empty, add new node to the rear
+        else:
+            self.rear.next = new_node
+            self.rear = new_node
+
+
+
+    def dequeue(self):
+        #removes the node from the front of the queue, and returns the node’s value.
+        if self.front is None:
+            raise ValueError("Queue is empty")
+
+        #get the value of the front
+        dequeue_value = self.front.value
+
+        #move the pointer to its next node
+        self.front = self.front.next
+
+        #queue is empty, so need to update rear - edge case
+        if self.front is None:
+            self.rear = None
+
+        return dequeue_value
+
+
+
+    def peek(self):
+        if self.front is None:
+            raise ValueError("Queue is empty")
+        return self.front.value
+
+
+
+    def is_empty(self):
+        return self.front is None
--- python/data_structures/stack.py
+++ python/data_structures/stack.py
@@ -1,12 +1,34 @@
+from data_structures.linked_list import Node
+
 class Stack:
-    """
-    Put docstring here
-    """
 
     def __init__(self):
-        # initialization here
-        pass
+        self.top = None
 
-    def some_method(self):
-        # method body here
-        pass
+
+    def push(self, value):
+
+        new_node = Node(value)
+        new_node.next = self.top
+        self.top = new_node
+
+
+    def pop(self):
+        if self.top is None:
+            raise ValueError("Stack is empty")
+
+        pop_value = self.top.value
+        #move the pointer which "removes" the node
+        self.top = self.top.next
+
+        return pop_value
+
+
+    def peek(self):
+        if self.top is None:
+            raise ValueError("Stack is empty")
+        return self.top.value
+
+
+    def is_empty(self):
+        return self.top is None

5212a8ef4760f6739abd536f81a4d5e8992cf45b
lana-z <lana@levelupeconomy.com>
2024-02-19 22:26:09 -0800

    start

--- python/code_challenges/hashtable_repeated_word.py
+++ python/code_challenges/hashtable_repeated_word.py
@@ -1,5 +1,22 @@
 from data_structures.hashtable import Hashtable
 
 
-def first_repeated_word():
-    pass
+def repeated_word():
+     hashtable = Hashtable()
+
+    # Normalize the input string and split it into words
+    words = s.lower().split()
+
+    # iterate through the words
+    for word in words:
+
+        if hashtable.has(word):
+            # since the word is already in the hashtable, it's the repeated word
+            return word
+
+        else:
+            # add the word to the hashtable with 1 for first occurrence
+            hashtable.set(word, 1)
+
+    # if no repeated word was found, return a message
+    return "No repeated word found."
